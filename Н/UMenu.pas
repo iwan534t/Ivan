a5cb5c1cbd4e11a8f7ebb7c2e0fbd4af39b34b0b
iwan <iwan534t@gmail.com>
2018-11-16 10:23:31 +0700

    g

---
+++ Н/UMenu.pas
@@ -0,0 +1,37 @@
+unit UMenu;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    h1: TMenuItem;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N11: TMenuItem;
+    N21: TMenuItem;
+    N31: TMenuItem;
+    N41: TMenuItem;
+    N51: TMenuItem;
+    N12: TMenuItem;
+    N22: TMenuItem;
+    N32: TMenuItem;
+    N42: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
